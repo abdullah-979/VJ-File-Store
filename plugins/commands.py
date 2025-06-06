c28c4362b3637821ad6d88269d6e14d6fe56f81e
Abdullah <78228041+abdullah-979@users.noreply.github.com>
2025-06-06 10:57:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -229,7 +229,7 @@ async def start(client, message):
         msg = await client.get_messages(LOG_CHANNEL, int(decode_file_id))
         if msg.media:
             media = getattr(msg, msg.media.value)
-            title = formate_file_name(media.file_name)
+            title = (media.file_name)
             size=get_size(media.file_size)
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
